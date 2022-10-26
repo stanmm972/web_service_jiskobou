810da02d54d15721110c07e9401e732a88a441cf
Frederic Maurice-madelon <fred972@Frederics-MacBook-Pro.local>
2022-10-26 09:35:55 -0400

    Update job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,6 @@
 class Job < ApplicationRecord
 
-    belongs_to :user
-
-    self.primary_key = 'user_id'
+    belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
 
     def user_full_name
         user.full_name
